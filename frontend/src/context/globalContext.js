e7b078256774c87591ff2c6d91b7b884e6615423
Sukish17 <165570197+Sukish17@users.noreply.github.com>
2024-10-23 10:23:05 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "http://localhost:3001/api/v1/";
+const BASE_URL = "https://expense-tracker-server-seven.vercel.app/";
 
 
 const GlobalContext = React.createContext()
